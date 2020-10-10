d472bbc0a7e0425596db24eaf0475526ad079d7c
SCRYARC <72644288+SCRYARC@users.noreply.github.com>
2020-10-09 23:38:43 -0700

    Add files via upload
    blah

---
+++ knight.ps1
@@ -0,0 +1,138 @@
+$i = 0
+
+while($true)
+{
+	$i++
+	Write-Host "3"
+	Sleep 1
+	Write-Host "2"
+	Sleep 1
+	Write-Host "1"
+	if (2 -gt $i)
+		{
+		Clear
+		Write-Host "Knight of Traveling: Hello potion seller I am going into battle and I want only your strongest potions"
+		Sleep 10
+		}
+	if (3 -gt $i)
+		{
+		Write-Host " "
+		Write-Host "Seller of Potions: My potions are too strong for you traveler"
+		Sleep 9
+		}
+	if (4 -gt $i)
+		{
+		Clear
+		Write-Host "Knight of Traveling: Potion seller, I tell you I'm going into battle and I want only your strongest potions"
+		Sleep 7
+		}
+	if (5 -gt $i)
+		{
+		Write-Host " "
+		Write-Host "Seller of Potions: You can't handle my strongest potions they are too strong for you"
+		Sleep 7
+		}
+	if (6 -gt $i)
+		{
+		Clear
+		Write-Host "Knight of Traveling: Potion seller, listen to me I want only your strongest potions"
+		Sleep 8
+		}
+	if (7 -gt $i)
+		{
+		Write-Host " "
+		Write-Host "Seller of Potions: My potions would kill you traveler, you cannot handle my potions"
+		Sleep 7
+		}
+	if (8 -gt $i)
+		{
+		Clear
+		Write-Host "Knight of Traveling: Potion seller enough of these games I'm going into battle and I need your strongest potions"
+		Sleep 9
+		}
+	if (9 -gt $i)
+		{
+		Write-Host " "
+		Write-Host "Seller of Potions: My strongest potions would kill you traveler, you can't handle my strongest potions you better go to a seller that sells weaker potions"
+		Sleep 9
+		}
+	if (10 -gt $i)
+		{
+		Clear
+		Write-Host "Knight of Traveling: Potion seller, I'm telling you right now, I'm going into battle and I need only your strongest potions"
+		Sleep 11
+		}
+	if (11 -gt $i)
+		{
+		Write-Host " "
+		Write-Host "Seller of Potions: You don't know what you ask traveler my strongest potions would kill a dragon, let alone a man, you need a seller that sells weaker potions cause my potions are too strong"
+		Sleep 16
+		}
+	if (12 -gt $i)
+		{
+		Clear
+		Write-Host "Knight of Traveling: Potion seller! I'm telling you! I need your strongest potions!! I'm going into battle, I'm going to battle and I need your strongest potions!!!"
+		Sleep 14
+		}
+	if (13 -gt $i)
+		{
+		Write-Host " "
+		Write-Host "Seller of Potions: You can't handle my strongest potions! No one can! My strongest potions are fit for the beasts, let alone a man"
+		Sleep 11
+		}
+	if (14 -gt $i)
+		{
+		Clear
+		Write-Host "Knight of Traveling: Potion seller what do I have to tell you to get your potions!! Why wont you trust me with your strongest potions I need them if I'm going to be successful in battle"
+		Sleep 13
+		}
+	if (15 -gt $i)
+		{
+		Write-Host " "
+		Write-Host "Seller of Potions: I cannot give you my strongest potions because they are only for the stongest beings and you are of the weakest"
+		Sleep 9
+		}
+	if (16 -gt $i)
+		{
+		Clear
+		Write-Host "Knight of Traveling: Well then that's it then potion seller, I'll go elsewhere! I'll go elsewhere for my potions!"
+		Sleep 8
+		}
+	if (17 -gt $i)
+		{
+		Write-Host " "
+		Write-Host "Seller of Potions: That's what you better do"
+		Sleep 2
+		}
+	if (18 -gt $i)
+		{
+		Clear
+		Write-Host "Knight of Traveling: I'll go elsewhere for my potions and never come back!"
+		Sleep 4
+		}
+	if (19 -gt $i)
+		{
+		Write-Host " "
+		Write-Host "Seller of Potions: Good you're not welcome here, my potions are only for the strongest and you are clearly of the weakest"
+		Sleep 9
+		}
+	if (20 -gt $i)
+		{
+		Clear
+		Write-Host "Knight of Traveling: You've have your say potion seller and ill have mine, you're a rascal, you're a rascal with no respect for knights! No respect for anything expcept your potions!!"
+		Sleep 13
+		}
+	if (21 -gt $i)
+		{
+		Write-Host " "
+		Write-Host "Seller of Potions: Why respect knights when my potions can do anything that you can"
+		Sleep 9
+		}
+	if (23 -gt $i)
+		{
+		Clear
+		Write-Host "*ZOOM IN*"
+		Sleep 3
+		Return $false
+		}
+}
